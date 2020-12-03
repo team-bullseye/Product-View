0285b12541effda898a32c2cff12c737a0df3007
Andrew McRitchie <andrewgmcritchie@gmail.com>
2020-12-02 18:35:09 -0800

    Added seeding and db importing script

---
+++ data/bigSeed.js
@@ -0,0 +1,38 @@
+const faker = require('faker');
+const fs = require('fs');
+const db = require('../db/index.js');
+const mongoose = require('mongoose');
+const csv = require('csvtojson');
+
+
+db.remove({})
+  .then((results) => {
+    console.log('Deleted all items')
+  })
+  .catch((err) => {
+    console.log('Did not delete items')
+  })
+
+var data = 'id,item,price,imgUrl,system\n';
+for (var i = 1; i < 100; i++) {
+  data += '' + i + ',' + faker.commerce.productName() + ',' + faker.commerce.price() + ',' +  faker.image.cats() + ',' + faker.vehicle.model() + '\n';
+}
+fs.appendFile('./data.csv', data, 'utf8', (err, results) => {
+  console.log('file appended')
+})
+
+csv()
+  .fromFile('./data.csv')
+  .then((jsonData)=> {
+    db.insertMany(jsonData)
+      .then((result) => {
+        console.log('Data seeded');
+        console.log('jsonData', jsonData);
+      })
+      .catch((err) => {
+        console.log('error seeding', err);
+      })
+  })
+  .catch((err) => {
+    console.log('didt not convert to json', err);
+  })
